6209ce3da86cbf36d2a011b206de1f1c3dc9fc03
sdlxrd <91081868+sdlxrd@users.noreply.github.com>
2022-05-09 17:38:26 +0300

    Update PR2_1.cpp
    last commit

--- PR2_1/PR2_1.cpp
+++ PR2_1/PR2_1.cpp
@@ -3,34 +3,24 @@
 
 using namespace std;
 
-IntRange makeIntRange(int x, int y)
-{
-	IntRange in;
-	if (!in.Init(x, y))
-		cout << "wrong argument to Init!" << endl;
-
-	return in;
-}
-
 int main()
 {
-	IntRange Read();
-	IntRange first(2, 7);
-	IntRange second(8, 10);
-	IntRange in;
-	int n;
+    IntRange first(1,5);
+    int n;
 
-	cout << "Number: "; cin >> n;
+    cout << first << endl;
+    cout << ++first << endl;
+    cout << first << endl;
+    cout << first++ << endl;
+    cout << first << endl;
 
-	cout << first++ << endl;
-	in.rangeCheck(n);
-	cout << first-- << endl;
-	in.rangeCheck(n);
-	cout << ++first << endl;
-	in.rangeCheck(n);
-	cout << --first << endl;
-	in.rangeCheck(n);
+    IntRange in;
+    cout << "Number: "; cin >> n;
+    in.rangeCheck(n);
 
-	cin.get();
-	return 0;
+    IntRange n();
+    IntRange b(first);
+
+    cin.get();
+    return 0;
 }
